162860fdcd4a1eb7db88c34caa25697abc370774
Pawel Gedlek <pawgedlek@gmail.com>
2025-08-07 22:28:30 +0200

    Create Project section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import AboutMe from "../components/AboutMe";
 import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
+import ProjectsSection from "../components/ProjectsSection";
 import SkillsSection from "../components/SkillsSection";
 import StarBackground from "../components/StarBackground";
 import ThemeToggle from "../components/ThemeToggle";
@@ -21,8 +22,15 @@ function Home() {
             <main>
                 {/* Hero section */}
                 <HeroSection />
+
+                {/* About me section */}
                 <AboutMe />
+
+                {/* Skills section */}
                 <SkillsSection />
+
+                {/* Projects section*/}
+                <ProjectsSection />
             </main>
 
             {/* Footer */}
