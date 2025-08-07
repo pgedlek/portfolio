549e698a064d8ba8b71833aec8dfc497f6a28269
Pawel Gedlek <pawgedlek@gmail.com>
2025-08-07 22:27:49 +0200

    Decrease Theme Toggle padding

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -31,7 +31,7 @@ function ThemeToggle() {
     return (
         <button onClick={toggleTheme}
             className={cn(
-                "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+                "fixed max-sm:hidden top-5 right-5 z-50 p-1 rounded-full transition-colors duration-300",
                 "focus:outlin-hidden"
             )}
         >
