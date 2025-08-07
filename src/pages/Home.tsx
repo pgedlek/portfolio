67ca2b3f45d5874b16b7b9779ad5b0e0dc8b62a5
Pawel Gedlek <pawgedlek@gmail.com>
2025-08-07 18:27:42 +0200

    Prepare tailwind CSS config

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,22 @@
 function Home() {
     return (
-        <>
-            Home
-        </>
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+            {/* Theme Toggle */}
+
+
+            {/* Background Effects */}
+
+
+            {/* Navbar */}
+
+
+            {/* Main Content */}
+
+
+            {/* Footer */}
+
+
+        </div>
     );
 }
 
